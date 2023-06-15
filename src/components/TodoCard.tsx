2d1a245cce7af0ec02daaa992c2dad98a9aa5757
jhchoi1182 <jhchoi1182@gmail.com>
2023-06-16 05:53:15 +0900

    refactor: 수정, 삭제 기능 속도 개선
    수정, 삭제 시 서버 요청 받아오던 로직을 전역 상태 관리값으로 변경해 렌더링 속도 개선
    
    Fixes: #1

--- src/components/TodoCard.tsx
+++ src/components/TodoCard.tsx
@@ -10,16 +10,17 @@ type TodoCardProps = {
 const BUTTON_STYLE = "w-32 h-10 border-2 rounded-lg shadow-sm";
 
 export default function TodoCard({ todo: { _id, title, contents, isDone } }: TodoCardProps) {
-  const { setTodos } = useContext(TodoContext);
-  const isDoneChange = async (id: string, isDone: boolean) => {
-    const responseTodo = await __updateTodo(id, isDone);
-    setTodos(responseTodo);
+  const { todos, setTodos } = useContext(TodoContext);
+  const isDoneChange = (id: string, isDone: boolean) => {
+    __updateTodo(id, isDone);
+    const changedTodos = todos.map((todo) => (todo._id === id ? { ...todo, isDone: !todo.isDone } : todo));
+    setTodos(changedTodos);
   };
 
-  const deleteTodo = async (id: string) => {
-    await __deleteTodo(id);
-    const responseTodo = await __getTodo();
-    setTodos(responseTodo);
+  const deleteTodo = (id: string) => {
+    __deleteTodo(id);
+    const deletedTodos = todos.filter((todo) => todo._id !== id);
+    setTodos(deletedTodos);
   };
 
   return (
