9dcf9097691a7d26c29c25637028d9833115237d
jhchoi1182 <jhchoi1182@gmail.com>
2024-01-04 19:55:11 +0900

    fix: 빌드 오류 수정

--- src/service/service.ts
+++ src/service/service.ts
@@ -1,14 +1,14 @@
 import { Todo } from "@/model/todo";
 import { checkEnvironment } from "./api";
 
-export async function getTodo(): Promise<Todo[]> {
+export async function getTodo(): Promise<Todo[] | []> {
   try {
     const response = await fetch(checkEnvironment() + "api/todo");
     if (!response.ok) {
       throw new Error("투두 불러오기에 실패했습니다.");
     }
     const { todos } = await response.json();
-    return todos;
+    return todos ?? [];
   } catch (error) {
     console.error("투두 GET :", error);
     throw new Error("투두 불러오기 중에 오류가 발생했습니다.");
