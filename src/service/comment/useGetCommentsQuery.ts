e77aed2366fdb45f3524a037b791f1bfa6305994
jhchoi1182 <jhchoi1182@gmail.com>
2024-04-18 00:01:29 +0900

    feat: 상세글 실시간 데이터와 페이지네이션 캐시 데이터 동기화 로직 추가

--- src/service/comment/useGetCommentsQuery.ts
+++ src/service/comment/useGetCommentsQuery.ts
@@ -3,25 +3,33 @@ import { QUERY_KEY } from "@/config/queyKeyConfig";
 import { setComments } from "@/stores/modules/commentSlice";
 import { RootState } from "@/stores/store/store";
 import { Comment } from "@/types/model/comment";
-import { useQuery } from "@tanstack/react-query";
+import { PostsResponse } from "@/types/response/postsResponse";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 function useGetCommentsQuery(postId: number) {
   const comments = useSelector(({ commentSlice }: RootState) => commentSlice.comments);
+  const queryClient = useQueryClient();
   const dispatch = useDispatch();
 
-  const { data, isLoading } = useQuery<Comment[]>({
+  const result = useQuery<Comment[]>({
     queryKey: [QUERY_KEY.comments, postId],
     queryFn: () => commentApi.get(postId),
   });
+  const { data } = result;
 
   useEffect(() => {
     if (!data) return;
     dispatch(setComments(data));
+    const currentPage = sessionStorage.getItem("currentPage") ?? 1;
+    const selectedCategory = sessionStorage.getItem("selectedCategory") ?? "전체";
+    const prevPosts = queryClient.getQueryData<PostsResponse>([QUERY_KEY.posts, selectedCategory, +currentPage]);
+    const updatedPosts = prevPosts?.content?.map((post) => (post.postId === postId ? { ...post, commentsCount: data?.length } : post));
+    queryClient.setQueryData([QUERY_KEY.posts, selectedCategory, +currentPage], { ...prevPosts, content: updatedPosts });
   }, [data]);
 
-  return { comments, isLoading };
+  return { comments, ...result };
 }
 
 export default useGetCommentsQuery;
