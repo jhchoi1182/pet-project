92fa12c6af879c83ce6507853d93df0b5f2a4947
jhchoi1182 <jhchoi1182@gmail.com>
2024-04-23 15:08:57 +0900

    chore: Playwright 환경 세팅

--- tests/unit/service/hooks/usePagination.test.tsx
+++ tests/unit/service/hooks/usePagination.test.tsx
@@ -8,7 +8,7 @@ const pagination = (currentPage: number, totalPages: number) => {
 };
 
 describe("usePagination 테스트", () => {
-  it("초기 렌더링 시 페이지는 1이어야 함", () => {
+  it("초기 렌더링 시 현재 페이지가 1이어야 함", () => {
     const currentPage = store.getState().postSlice.currentPage;
     const { result } = pagination(currentPage, 100);
     expect(result.current.pages[0]).toEqual(1);
@@ -19,14 +19,14 @@ describe("usePagination 테스트", () => {
     expect(result.current.pages).toEqual(Array.from({ length: 13 }, (_, i) => i + 1));
   });
 
-  it("14페이지로 이동하면 현재 페이지가 14페이지어야 함", () => {
+  it("14페이지로 이동하면 현재 페이지가 14페이지여야 함", () => {
     const { result } = pagination(1, 100);
     act(() => result.current.movePage(14));
     const currentPage = store.getState().postSlice.currentPage;
     expect(currentPage).toEqual(14);
   });
 
-  it("페이지 이동 시 세션스토리지에 이동한 페이지가 저장되어야 함", () => {
+  it("페이지 이동 시 세션 스토리지에 이동한 페이지가 저장되어야 함", () => {
     const { result } = pagination(1, 100);
     act(() => result.current.movePage(5));
     expect(window.sessionStorage.getItem("currentPage")).toBe("5");
@@ -43,7 +43,7 @@ describe("usePagination 테스트", () => {
   });
 
   it("totalPages가 0일 때 pages가 빈 배열이어야 함", () => {
-    const { result } = pagination(10, 0);
+    const { result } = pagination(1, 0);
     expect(result.current.pages).toEqual([]);
   });
 });
